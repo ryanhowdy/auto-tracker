070b2b0b7e3de0708f86e20a747a4d007b86e79c
Haudenschilt <rhaudenschilt@contactusinc.com>
2026-01-13 11:14:52 -0500

    Added better dashboard items.

---
+++ app/Filament/Widgets/VehicleMileageOverview.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\Mile;
+use Illuminate\Support\Facades\DB;
+
+class VehicleMileageOverview extends StatsOverviewWidget
+{
+    protected ?string $heading = 'Current Mileage';
+
+    protected function getStats(): array
+    {
+        $milesPerCar = DB::table('miles as m')
+            ->select('m.*', 'v.name')
+            ->leftJoin('miles as m1', function ($join) {
+                $join->on('m.vehicle_id', '=', 'm1.vehicle_id')
+                    ->whereRaw(DB::raw('m.created_at < m1.created_at'));
+            })
+            ->leftJoin('vehicles as v', 'm.vehicle_id', '=', 'v.id')
+            ->whereNull('m1.vehicle_id')
+            ->get();
+
+        $stats = [];
+
+        foreach($milesPerCar as $car)
+        {
+            $stats[] = Stat::make($car->name, $car->miles);
+        }
+
+        return $stats;
+    }
+}
---
+++ app/Filament/Widgets/VehicleYearCostOverview.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\ServiceHistory;
+use Carbon\Carbon;
+
+class VehicleYearCostOverview extends StatsOverviewWidget
+{
+    protected ?string $heading = 'Yearly Costs';
+
+    protected function getStats(): array
+    {
+        $costsByCar = ServiceHistory::whereYear('created_at', '>=', Carbon::now()->year)
+            ->with('vehicle')
+            ->get()
+            ->groupBy('vehicle_id');
+
+        $stats  = [];
+        $totals = [];
+
+        foreach($costsByCar as $carId => $costs)
+        {
+            $carName = $costs[0]->vehicle->name;
+
+            foreach($costs as $i => $cost)
+            {
+                $totals[$i] = isset($totals[$i]) ? $totals[$i] + $cost->total : $cost->total;
+            }
+        }
+
+        foreach($totals as $i => $total)
+        {
+            $stats[] = Stat::make($carName, $total)
+                ->descriptionIcon('heroicon-s-currency-dollar');
+        }
+
+        return $stats;
+    }
+}
